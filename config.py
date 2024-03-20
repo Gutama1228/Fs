0bc714ca767b76e8706102e2f084f377caa53581
gutama <142733438+Gutama1228@users.noreply.github.com>
2024-03-20 13:20:18 +0700

    update config.py

--- config.py
+++ config.py
@@ -36,7 +36,7 @@ GROUP = os.environ.get("GROUP", "Basecampsupport")
 # ID dari Channel Atau Group Untuk Wajib Subscribenya
 FORCE_SUB_CHANNEL1 = int(os.environ.get("FORCE_SUB_CHANNEL1", "-1002047062577"))
 FORCE_SUB_CHANNEL2 = int(os.environ.get("FORCE_SUB_CHANNEL2", "-1001674494092"))
-FORCE_SUB_CHANNEL3 = int(os.environ.get("FORCE_SUB_CHANNEL3", "-1001674494092"))
+FORCE_SUB_CHANNEL3 = int(os.environ.get("FORCE_SUB_CHANNEL3", "2104757459"))
 FORCE_SUB_CHANNEL4 = int(os.environ.get("FORCE_SUB_CHANNEL4", "-1002040495578"))
 
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
